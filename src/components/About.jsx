bb5bad256e72b5147fe5463c46af1f478d0a97a0
rEZe <soklavorteysiek@gmail.com>
2026-08-18 01:50:08 +0700

    fix image import path

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,22 +1,32 @@
+import React from 'react';
+import aboutImg from '../assets/about-img.jpg';
+
 export default function About() {
   return (
     <section className="about-section" id="about">
       <div className="section-content">
         <div className="about-image-wrapper">
-        <img  src={`${import.meta.env.BASE_URL}img/about-img.png`}  alt="About our coffee shop" />
+          <img src={aboutImg} alt="About our coffee shop" />
         </div>
         <div className="about-details">
-          <h2 className="section-title">About Us</h2>
+          <h2 className="section-title">ABOUT US</h2>
           <p className="text">
-            At Coffee House in PhnomPenh, Cambodia, we pride ourselves on being a go-to
-            destination for coffee lovers and conversation seekers alike. We're dedicated to
-            providing an exceptional coffee experience in a cozy and inviting atmosphere,
-            where guests can relax, unwind, and enjoy their time in comfort.
+            At Coffee House in PhnomPenh, Cambodia, we pride ourselves on
+            being a go-to destination for coffee lovers and conversation seekers
+            alike. We're dedicated to providing an exceptional coffee experience
+            in a cozy and inviting atmosphere, where guests can relax, unwind,
+            and enjoy their time in comfort.
           </p>
           <div className="social-link-list">
-            <a href="#" className="social-link" aria-label="Facebook"><i className="fa-brands fa-facebook" /></a>
-            <a href="#" className="social-link" aria-label="Instagram"><i className="fa-brands fa-instagram" /></a>
-            <a href="#" className="social-link" aria-label="TikTok"><i className="fa-brands fa-tiktok" /></a>
+            <a href="#" className="social-link" aria-label="Facebook">
+              <i className="fa-brands fa-facebook" />
+            </a>
+            <a href="#" className="social-link" aria-label="Instagram">
+              <i className="fa-brands fa-instagram" />
+            </a>
+            <a href="#" className="social-link" aria-label="TikTok">
+              <i className="fa-brands fa-tiktok" />
+            </a>
           </div>
         </div>
       </div>
